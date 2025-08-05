a849019057e58ba07809b33b3ab831ef2f2a4107
jkchoy <jackilyn@hawaii.edu>
2025-08-05 13:46:21 -1000

    save cart in local storage

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,12 +1,21 @@
-export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quanity: 2
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quanity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quanity: 2
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quanity: 1
+  }];
+}
+
 // added text="module" to amazon.html scrpt: <script type="module" src="scripts/amazon.js"></script>
 // added import { cart } from '../data/cart.js'; to amazon.js
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -25,6 +34,8 @@ export function addToCart(productId) {
       quanity: 1
     });
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -37,4 +48,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 }

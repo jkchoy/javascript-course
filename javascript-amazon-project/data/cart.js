73c3d3ec671b51eba640e842867c7971e7489a43
jkchoy <jackilyn@hawaii.edu>
2025-08-04 15:54:19 -1000

    added modules

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,1 +1,3 @@
-const cart = [];
+export const cart = [];
+// added text="module" to amazon.html scrpt: <script type="module" src="scripts/amazon.js"></script>
+// added import { cart } from '../data/cart.js'; to amazon.js

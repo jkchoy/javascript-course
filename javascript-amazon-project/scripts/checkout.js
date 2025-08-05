81346daaddb35842257f42c24c01631bae50570b
jkchoy <jackilyn@hawaii.edu>
2025-08-05 12:58:46 -1000

    matching product id link

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -72,7 +72,7 @@ cart.forEach((cartItem) => {
                 </div>
               </div>
               <div class="delivery-option">
-                <input type="radio" class="delivery-option-input" name="${matchingProduct.id}">
+                <input type="radio" class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
                 <div>
                   <div class="delivery-option-date">
                     Monday, June 13

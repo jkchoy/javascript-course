a61f416305e4d5009226c957268c8803b7d6cf79
jkchoy <jackilyn@hawaii.edu>
2025-08-05 12:19:08 -1000

    generate html for order summary

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,4 +1,10 @@
-export const cart = [];
+export const cart = [{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quanity: 2
+}, {
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quanity: 1
+}];
 // added text="module" to amazon.html scrpt: <script type="module" src="scripts/amazon.js"></script>
 // added import { cart } from '../data/cart.js'; to amazon.js
 

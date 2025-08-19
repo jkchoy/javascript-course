c1cf52f460d987ed8a6a09fb713043331cc0da2e
jkchoy <jackilyn@hawaii.edu>
2025-08-19 11:33:15 -1000

    learn promises and convert code to promises

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -75,3 +75,18 @@ export function updateDeliverOption(productId, deliveryOptionId) {
 
   saveToStorage();
 }
+
+export let products = [];
+
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response);
+
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+}
